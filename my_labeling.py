dfae8dc6fe4820cda8ab34a0c1d9154788abec36
chachades <cha.desante@gmail.com>
2024-05-10 10:33:58 +0200

    I have coded the two first functions of the qualitative analysis

--- my_labeling.py
+++ my_labeling.py
@@ -1,7 +1,8 @@
-__authors__ = 'TO_BE_FILLED'
-__group__ = 'TO_BE_FILLED'
+__authors__ = ['1716921', '1718541', '1720318']
+__group__ = '213'
 
 from utils_data import read_dataset, read_extended_dataset, crop_images
+import numpy as np
 
 
 if __name__ == '__main__':
@@ -18,3 +19,28 @@ if __name__ == '__main__':
     cropped_images = crop_images(imgs, upper, lower)
 
     # You can start coding your functions here
+    def Retrival_by_colour(list_images, tags, question, percentage=None):
+        possible_images = []
+        list_percentages = []
+        for i in range(np.shape(tags)[0]):
+            if list(question) in tags[i, :]:
+                possible_images.append(list_images[i])
+                if percentage is not None:
+                    list_percentages.append(np.mean([percentage[i][j] for j in list(question)]))
+        if percentage is not None:
+            possible_images = [images for percentage, images in sorted(zip(list_percentages, possible_images))]
+        return possible_images
+
+    def Retrival_by_shape(list_images, tags, question, percentage=None):
+        possible_images = []
+        list_percentages = []
+        for i in range(np.shape(tags)[0]):
+            if question in tags[i, :]:
+                possible_images.append(list_images[i])
+                if percentage is not None:
+                    list_percentages.append(percentage[i][question])
+        if percentage is not None:
+            possible_images = [images for percentage, images in sorted(zip(list_percentages, possible_images))]
+        return possible_images
+
+
